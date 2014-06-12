8012975d6c7ab4d32265c65030523e850b58bd3f
DwayneTen <151809668@qq.com>
2014-06-12 16:32:47 +0800

    ver 1.10
    ver 1.10
    Add:
    1.Add counter.
    Improve:
    1.Change usage text color.
    2.Update infomation after each operation by one line.
    3.Remove vbs infomation.
    Fix:
    1.Error occurs when delete files.

--- FontInstaller.py
+++ FontInstaller.py
@@ -11,7 +11,7 @@ class gui:
 	def __init__(self):
 		window = Tk()
 		# title
-		window.title('Font Installer Beta 1.0')
+		window.title('Font Installer 1.10')
 		# size
 		window.geometry('450x280')
 		# icon
@@ -87,12 +87,19 @@ class gui:
 		scrollbar.pack(side = RIGHT, fill = Y)
 		self.info = Text(frameInfo, width = 150, height = 60, yscrollcommand = scrollbar.set)
 		self.info.pack()
-		self.info.insert(END, 'Here are many unexpected bug becouse of beta version.\nPleae backup before use.\nUse Step:\n1.Select font root path.\n2.Select target root path.\n3.Extract.\n4.Install.\n\n')
+		self.info.config(font = 'Courier 10')
+		self.info.tag_config('blue', foreground = 'blue')
+		self.info.insert(END, '-'*50+'\n')
+		self.info.insert(END, 'Pleae backup before use.\nUseage:\n1.Select font root path.\n2.Select target root path.\n3.Extract or Install.\n', 'blue')
+		self.info.insert(END, '-'*50+'\n')
 		scrollbar.config(command = self.info.yview)
 
 		window.mainloop()
 
 	def extract(self):
+		"""Extract fonts from fontPath"""
+		
+		self.couterExtracted = 0
 		if not os.path.isdir(self.targetPath.get()):
 			os.makedirs(self.targetPath.get())
 		fontPath = self.fontPath.get()
@@ -104,13 +111,21 @@ class gui:
 					sufix = os.path.splitext(fn)[1][1:]
 					if sufix == 'zip' or sufix == 'ZIP':
 						self.info.insert(END, 'Extracted: '+fn+'\n')
+						self.info.update()
 						f = zipfile.ZipFile(root+'\\'+fn)
 						if self.judTarget.get():
 							f.extractall(self.targetPath.get()+'\\')
 						else:
 							f.extractall(root+'\\')
+						self.couterExtracted += 1
+		self.info.insert(END, '-'*50+'\n')
+		self.info.insert(END, 'Extracted'+' '+str(self.couterExtracted)+' Zips.'+'\n')
 
 	def install(self):
+		"""Install fonts from fontPath"""
+
+		couterInstalled = 0
+		couterDeleted = 0
 		fontPath = self.fontPath.get()
 		if self.judTarget.get():
 			fontPath = self.targetPath.get() 
@@ -128,15 +143,20 @@ class gui:
 						else:
 							temp += c
 					os.system('Installfont.vbs'+' '+temp+'\\'+fn)
-					self.info.insert(END, 'Installfont.vbs'+' '+temp+'\\'+fn+'\n'+'Installed: '+fn+'\n')
-					if isDelete:
-						os.remove(root+'\\'+fn)
-						self.info.insert(End, '')
+					self.info.insert(END, 'Installed: '+fn+'\n')
+					self.info.update()
+					couterInstalled += 1
 				elif sufix == 'zip' or sufix == 'ZIP' or sufix == 'rar' or sufix == 'RAR':
 					pass
 				elif self.judDelete.get():
 					os.remove(root+'\\'+fn)
 					self.info.insert(END, 'Deleted: '+fn+'\n')
+					self.info.update()
+					couterDeleted += 1
+		self.info.insert(END, '-'*50+'\n')
+		self.info.insert(END, 'Installed'+' '+str(couterInstalled)+' Fonts.'+'\n')
+		self.info.insert(END, 'Deleted'+' '+str(couterDeleted)+' Files.'+'\n')
+		self.info.update()
 
 	def dialogAbout(self):
 		pass
@@ -145,13 +165,19 @@ class gui:
 		pass
 
 	def popup(self, event):
+		"""Popup menu"""
+
 		self.popupMenu.post(event.x_root, event.y_root)
 
 	def fontPathSelect(self):
+		"""Select fontPath"""
+
 		fontPath = askdirectory(initialdir="/",title='Pick a directory')
 		self.fontPath.set(fontPath)
 
 	def targetPathSelector(self):
+		"""Select targetPath"""
+
 		targetPath = askdirectory(initialdir="/",title='Pick a directory')
 		self.targetPath.set(targetPath)
 
